ab3b31c5b6b51fa16a8bf0e185ccb0e77e2a00b2
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2019-06-17 19:33:58 -0400

    images are now the same size and centered

--- src/components/characters.js
+++ src/components/characters.js
@@ -1,30 +1,84 @@
 import React from 'react';
 import { Row, Container, Col } from 'react-bootstrap';
 
+import '../App.css';
+
 const CharacterStyles = {
     color: 'white',
     borderColor: 'white',
     borderWidth: '2px',
-    borderStyle: 'solid'
+    borderStyle: 'solid',
+    characterImage: {
+        width: '200px',
+        height: 'auto'
+    }
 }
+
+
 const Characters = props => {
     // console.log(props.characters[0]);
+    let image_holder1 = [];
+    let image_holder2 = [];
+    let image_holder3 = [];
+
+
+    props.characters.forEach((character, idx) => {
+        if(idx <= 2){
+            image_holder1.push(
+                <Col lg={3} key={idx}>
+                    <img 
+                        src={character.src} 
+                        alt={character.alt} 
+                        className='character_image mt-4'
+                        style={CharacterStyles.characterImage}
+                    />
+                </Col>
+            )
+        }else if(idx > 2 && idx <= 5){
+            image_holder2.push(
+                <Col lg={3} key={idx}>
+                    <img 
+                        src={character.src} 
+                        alt={character.alt} 
+                        className='character_image mt-4'
+                        style={CharacterStyles.characterImage}
+                    />
+                </Col>
+            )
+        }else if(idx > 5){
+            image_holder3.push(
+                <Col lg={3} key={idx}>
+                    <img 
+                        src={character.src} 
+                        alt={character.alt} 
+                        className='character_image mt-4'
+                        style={CharacterStyles.characterImage}
+                    />
+                </Col>
+            )
+        }
+    })
+
     return (
         <div style={CharacterStyles} className='mt-5 mx-auto text-center'>
             <Container>
-                {/* <img src={props.characters[0].src} alt={props.characters[0].alt} /> */}
-                <Row>
-                    {props.characters.map((character, idx) => {
-                        return (
+               
 
-                            <Col lg={3} key={idx}>
-                                <img src={character.src} alt={character.alt} />
-                            </Col>
+                {image_holder1.length === 3 && image_holder2.length === 3 && image_holder1.length === 3 &&
+                    <div>
+                        <Row className='justify-content-center'>
+                            {image_holder1}
+                        </Row>
+                        <Row className='justify-content-center'>
+                            {image_holder2}
+                        </Row>
+                        <Row className='justify-content-center'>
+                            {image_holder3}
+                        </Row>
+                    </div>
+                }
 
-                        )
-                    })}
 
-                </Row>
             </Container>
 
         </div>
